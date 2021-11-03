0afccbff43505c69279ef13dfff2746e09d9d394
caca66 <93549340+caca66@users.noreply.github.com>
2021-11-03 15:35:07 +0300

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -102,8 +102,8 @@ temp = """{ALIVE_TEXT}
 async def amireallyalive(event):
     "A kind of showing bot details by your inline bot"
     reply_to_id = await reply_id(event)
-    EMOJI = gvarstatus("ALIVE_EMOJI") or "✧✧"
-    ALIVE_TEXT = gvarstatus("ALIVE_TEXT") or "**Catuserbot is Up and Running**"
+    EMOJI = gvarstatus("ALIVE_EMOJI") or "*"
+    ALIVE_TEXT = gvarstatus("ALIVE_TEXT") or "**- هلا عيني\n- هاذي معلومات بوتك **"
     cat_caption = f"{ALIVE_TEXT}\n"
     cat_caption += f"**{EMOJI} Telethon version :** `{version.__version__}\n`"
     cat_caption += f"**{EMOJI} Catuserbot Version :** `{catversion}`\n"
